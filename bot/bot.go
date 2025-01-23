80a93eade260fdeb5937ea45b7cf0a0ba21b0d8a
Innokentiy Sokolov <keshon@zoho.com>
2025-01-23 19:40:20 +0300

    rework playback status handler

--- bot/bot.go
+++ bot/bot.go
@@ -80,7 +80,6 @@ func (b *Bot) configureIntents() {
 func (b *Bot) registerEventHandlers() {
 	b.session.AddHandler(b.onReady)
 	b.session.AddHandler(b.onMessageCreate)
-	b.session.AddHandler(b.onPlayback)
 }
 
 // Event Handlers
@@ -123,70 +122,6 @@ func (b *Bot) onMessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) 
 	}
 
 	executeCommand(s, m, b, cmd.Name, param)
-}
-
-func (b *Bot) onPlayback(s *discordgo.Session, m *discordgo.MessageCreate) {
-	if m.Author.ID == s.State.User.ID {
-		return
-	}
-
-	var currentChannelID string
-	if b.playChannelID[m.GuildID] != "" {
-		currentChannelID = b.playChannelID[m.GuildID]
-	} else {
-		currentChannelID = m.ChannelID
-	}
-
-	instance := b.getOrCreatePlayer(m.GuildID)
-	signal := <-instance.StatusSignals
-	switch signal {
-	case player.StatusPlaying:
-		if instance.Song == nil {
-			s.ChannelMessageSendEmbed(currentChannelID, embed.NewEmbed().SetColor(embedColor).SetDescription("No song is currently playing.").MessageEmbed)
-			return
-		}
-
-		emb := embed.NewEmbed().SetColor(embedColor)
-		title, source, publicLink, parser, err := instance.Song.GetSongInfo(instance.Song)
-		if err != nil {
-			s.ChannelMessageEditEmbed(currentChannelID, b.playMessage[m.GuildID].ID, emb.SetDescription(fmt.Sprintf("Error getting this song(s)\n\n%v", err)).MessageEmbed)
-		}
-		hostname, err := extractHostname(instance.Song.PublicLink)
-		if err != nil {
-			hostname = source
-		}
-		ffmpeg := "`ffmpeg`"
-		if parser != "" {
-			if parser == songpkg.ParserKkdai.String() {
-				parser = fmt.Sprintf("`%s`", "kkdai")
-			} else if parser == songpkg.ParserYtdlp.String() {
-				parser = fmt.Sprintf("`%s`", "ytdlp")
-			}
-		}
-		emb.SetDescription(fmt.Sprintf("%s Now playing\n\n**%s**\n[%s](%s)\n\n%s %s", player.StatusPlaying.StringEmoji(), title, hostname, publicLink, ffmpeg, parser))
-		if len(instance.Song.Thumbnail.URL) > 0 {
-			emb.SetThumbnail(instance.Song.Thumbnail.URL)
-		}
-		emb.SetFooter(fmt.Sprintf("Use %shelp for a list of commands.", b.prefixCache[m.GuildID]))
-		if b.playMessage[m.GuildID] != nil {
-			s.ChannelMessageEditEmbed(currentChannelID, b.playMessage[m.GuildID].ID, emb.MessageEmbed)
-		} else {
-			s.ChannelMessageSendEmbed(currentChannelID, emb.MessageEmbed)
-		}
-	case player.StatusResuming:
-		fmt.Println("Interuption detected, resuming...")
-	case player.StatusError:
-		fmt.Println("Error:", signal)
-	case player.StatusAdded:
-		desc := fmt.Sprintf("Song(s) added to queue\n\nUse `%slist` to see the current queue.", b.prefixCache[m.GuildID])
-		if b.playMessage[m.GuildID] != nil {
-			s.ChannelMessageEditEmbed(currentChannelID, b.playMessage[m.GuildID].ID, embed.NewEmbed().SetColor(embedColor).SetDescription(desc).MessageEmbed)
-		} else {
-			s.ChannelMessageSendEmbed(currentChannelID, embed.NewEmbed().SetColor(embedColor).SetDescription(desc).MessageEmbed)
-		}
-
-	}
-
 }
 
 // Utility Methods
--- bot/cmd_playback.go
+++ bot/cmd_playback.go
@@ -66,6 +66,32 @@ func playCommand(s *discordgo.Session, m *discordgo.MessageCreate, b *Bot, comma
 		return
 	}
 
+	go func() {
+		if m.Author.ID == s.State.User.ID {
+			return
+		}
+		instance := b.getOrCreatePlayer(m.GuildID)
+		for {
+			signal := <-instance.StatusSignals
+			switch signal {
+			case player.StatusPlaying:
+				playbackStatus(s, m, b)
+			case player.StatusResuming:
+				fmt.Println("Interuption detected, resuming...")
+			case player.StatusError:
+				fmt.Println("Error:", signal)
+			case player.StatusAdded:
+				desc := fmt.Sprintf("Song(s) added to queue\n\nUse `%slist` to see the current queue.", b.prefixCache[m.GuildID])
+				if b.playMessage[m.GuildID] != nil {
+					s.ChannelMessageEditEmbed(b.playChannelID[m.GuildID], b.playMessage[m.GuildID].ID, embed.NewEmbed().SetColor(embedColor).SetDescription(desc).MessageEmbed)
+				} else {
+					s.ChannelMessageSendEmbed(b.playChannelID[m.GuildID], embed.NewEmbed().SetColor(embedColor).SetDescription(desc).MessageEmbed)
+				}
+
+			}
+		}
+	}()
+
 	err = instance.Play()
 	if err != nil {
 		s.ChannelMessageEditEmbed(m.ChannelID, b.playMessage[m.GuildID].ID, emb.SetDescription(fmt.Sprintf("Error playing this song(s)\n\n%v", err)).MessageEmbed)
@@ -84,4 +110,41 @@ func skipCommand(s *discordgo.Session, m *discordgo.MessageCreate, b *Bot, comma
 func stopCommand(s *discordgo.Session, m *discordgo.MessageCreate, b *Bot, command, param string) {
 	instance := b.getOrCreatePlayer(m.GuildID)
 	instance.ActionSignals <- player.ActionStop
+}
+
+func playbackStatus(s *discordgo.Session, m *discordgo.MessageCreate, b *Bot) {
+	instance := b.getOrCreatePlayer(m.GuildID)
+
+	if instance.Song == nil {
+		s.ChannelMessageSendEmbed(b.playChannelID[m.GuildID], embed.NewEmbed().SetColor(embedColor).SetDescription("No song is currently playing.").MessageEmbed)
+		return
+	}
+
+	emb := embed.NewEmbed().SetColor(embedColor)
+	title, source, publicLink, parser, err := instance.Song.GetSongInfo(instance.Song)
+	if err != nil {
+		s.ChannelMessageEditEmbed(b.playChannelID[m.GuildID], b.playMessage[m.GuildID].ID, emb.SetDescription(fmt.Sprintf("Error getting this song(s)\n\n%v", err)).MessageEmbed)
+	}
+	hostname, err := extractHostname(instance.Song.PublicLink)
+	if err != nil {
+		hostname = source
+	}
+	ffmpeg := "`ffmpeg`"
+	if parser != "" {
+		if parser == songpkg.ParserKkdai.String() {
+			parser = fmt.Sprintf("`%s`", "kkdai")
+		} else if parser == songpkg.ParserYtdlp.String() {
+			parser = fmt.Sprintf("`%s`", "ytdlp")
+		}
+	}
+	emb.SetDescription(fmt.Sprintf("%s Now playing\n\n**%s**\n[%s](%s)\n\n%s %s", player.StatusPlaying.StringEmoji(), title, hostname, publicLink, ffmpeg, parser))
+	if len(instance.Song.Thumbnail.URL) > 0 {
+		emb.SetThumbnail(instance.Song.Thumbnail.URL)
+	}
+	emb.SetFooter(fmt.Sprintf("Use %shelp for a list of commands.", b.prefixCache[m.GuildID]))
+	if b.playMessage[m.GuildID] != nil {
+		s.ChannelMessageEditEmbed(b.playChannelID[m.GuildID], b.playMessage[m.GuildID].ID, emb.MessageEmbed)
+	} else {
+		s.ChannelMessageSendEmbed(b.playChannelID[m.GuildID], emb.MessageEmbed)
+	}
 }
